e5ee64cd54fc4fa5ff4334c42d02ef547f07e9bf
Joshua Moyers <jmoyers@gmail.com>
2011-06-18 16:20:54 -0700

    tab to space

--- src/event.hpp
+++ src/event.hpp
@@ -10,121 +10,121 @@ using namespace std;
 template <class T>
 class EventEmitter {
 public:
-	typedef boost::function<void (T)> listener_type;
-	typedef vector<listener_type> listener_list;
+   typedef boost::function<void (T)> listener_type;
+   typedef vector<listener_type> listener_list;
 
-	struct Node;
-	typedef map<string, Node> listener_node;
-	
-	struct Ghost {
-		vector<string> markers;
-		listener_type listener;
-	} ghost_t;
-	
-	struct Node {
-		string name;
-		listener_node branches;
-		listener_list listeners;
-		vector<Ghost *> wanderers;
-	} node_t;	
-	
-	EventEmitter(){};
-	~EventEmitter(){};
-	
-	void addListener(string channel, boost::function<void (T)> listener){		
-		vector<string> channels;
-		boost::split(channels, channel, boost::is_any_of("."));
-		
-		Node *currNode = &this->listeners;
-		
-		for (vector<string>::iterator i = channels.begin(); i != channels.end(); i++) {
-			Ghost *ghost = new Ghost();
-			
-			ghost->markers.resize(distance(i+1, channels.end()));
-			copy(i+1, channels.end(), ghost->markers.begin());
-			
-			ghost->listener = listener;
-			
-			if (*i == "*") {
-				currNode->wanderers.push_back(ghost);
-				return;
-			} else {
-				currNode = &currNode->branches[*i];
-				currNode->name = *i;
-			}
-		}
-		
-		currNode->listeners.push_back(listener);
-	}
-	
-	void emit(string channel, T payload){
-		listener_list list;
-		
-		this->getListeners(list, channel);
-		
-		for(typename listener_list::iterator i = list.begin(); i != list.end(); i++) {
-			((listener_type)*i)(payload);
-		}
-	}
-	
-	void getListeners(listener_list &listeners, string channel) {
-		vector<string> channels;
-		boost::split(channels, channel, boost::is_any_of("."));
+   struct Node;
+   typedef map<string, Node> listener_node;
+   
+   struct Ghost {
+      vector<string> markers;
+      listener_type listener;
+   } ghost_t;
+   
+   struct Node {
+      string name;
+      listener_node branches;
+      listener_list listeners;
+      vector<Ghost *> wanderers;
+   } node_t;   
+   
+   EventEmitter(){};
+   ~EventEmitter(){};
+   
+   void addListener(string channel, boost::function<void (T)> listener){      
+      vector<string> channels;
+      boost::split(channels, channel, boost::is_any_of("."));
+      
+      Node *currNode = &this->listeners;
+      
+      for (vector<string>::iterator i = channels.begin(); i != channels.end(); i++) {
+         Ghost *ghost = new Ghost();
+         
+         ghost->markers.resize(distance(i+1, channels.end()));
+         copy(i+1, channels.end(), ghost->markers.begin());
+         
+         ghost->listener = listener;
+         
+         if (*i == "*") {
+            currNode->wanderers.push_back(ghost);
+            return;
+         } else {
+            currNode = &currNode->branches[*i];
+            currNode->name = *i;
+         }
+      }
+      
+      currNode->listeners.push_back(listener);
+   }
+   
+   void emit(string channel, T payload){
+      listener_list list;
+      
+      this->getListeners(list, channel);
+      
+      for(typename listener_list::iterator i = list.begin(); i != list.end(); i++) {
+         ((listener_type)*i)(payload);
+      }
+   }
+   
+   void getListeners(listener_list &listeners, string channel) {
+      vector<string> channels;
+      boost::split(channels, channel, boost::is_any_of("."));
 
-		Node *currNode = &this->listeners;
-		bool found = false;
-		
-		for (vector<string>::iterator i = channels.begin(); i != channels.end(); i++) {
-			found = false;
-			if (currNode->branches.find(*i) != currNode->branches.end()) {
-				for (typename vector<Ghost *>::iterator ii = currNode->wanderers.begin(); ii != currNode->wanderers.end(); ii++) {
-					if (this->compareMarkers((*ii)->markers, channels)) {
-						listeners.push_back((*ii)->listener);
-					}
-				}
-				found = true;
-				currNode = &currNode->branches[*i];
-			} else {
-				return;
-			}
-		}
-		
-		if (found) {
-			listeners.insert(listeners.end(), currNode->listeners.begin(), currNode->listeners.end());
-		}
-	}
+      Node *currNode = &this->listeners;
+      bool found = false;
+      
+      for (vector<string>::iterator i = channels.begin(); i != channels.end(); i++) {
+         found = false;
+         if (currNode->branches.find(*i) != currNode->branches.end()) {
+            for (typename vector<Ghost *>::iterator ii = currNode->wanderers.begin(); ii != currNode->wanderers.end(); ii++) {
+               if (this->compareMarkers((*ii)->markers, channels)) {
+                  listeners.push_back((*ii)->listener);
+               }
+            }
+            found = true;
+            currNode = &currNode->branches[*i];
+         } else {
+            return;
+         }
+      }
+      
+      if (found) {
+         listeners.insert(listeners.end(), currNode->listeners.begin(), currNode->listeners.end());
+      }
+   }
 private:
-	Node listeners;
-	
-	bool compareMarkers(vector<string> &a, vector<string> &b){
-		int ai = a.size() - 1;
+   Node listeners;
+   
+   bool compareMarkers(vector<string> &a, vector<string> &b){
+      int ai = a.size() - 1;
 
-		if (ai < 0)
-			return true;
-			
-		int bi = b.size() - 1;
-		
-		for (;ai >= 0 && bi >= 0; ai--, bi--){
-			if (a[ai] != b[bi]) {
-				return false;
-			}
-		}	
-		
-		return true;
-	}
-	
-	void printNode(Node &node) {
-		cout << endl << "==== Node" << endl;
-		cout << "Node name     : " << node.name << endl;
-		cout << "Listener count: " << node.listeners.size() << endl;
-		cout << "Branch count  : " << node.branches.size() << endl;
-		cout << "Wanderer count: " << node.wanderer.size() << endl;
-	}
-	
-	void printVector(vector<string> &v) {
-		cout << endl << "==== Vector " << endl;
-		for (vector<string>::iterator i = v.begin(); i != v.end(); i++) {
-			cout << *i << endl;
-		}
-	}
+      if (ai < 0)
+         return true;
+         
+      int bi = b.size() - 1;
+      
+      for (;ai >= 0 && bi >= 0; ai--, bi--){
+         if (a[ai] != b[bi]) {
+            return false;
+         }
+      }   
+      
+      return true;
+   }
+   
+   void printNode(Node &node) {
+      cout << endl << "==== Node" << endl;
+      cout << "Node name     : " << node.name << endl;
+      cout << "Listener count: " << node.listeners.size() << endl;
+      cout << "Branch count  : " << node.branches.size() << endl;
+      cout << "Wanderer count: " << node.wanderer.size() << endl;
+   }
+   
+   void printVector(vector<string> &v) {
+      cout << endl << "==== Vector " << endl;
+      for (vector<string>::iterator i = v.begin(); i != v.end(); i++) {
+         cout << *i << endl;
+      }
+   }
 };
--- src/test.cpp
+++ src/test.cpp
@@ -6,31 +6,31 @@
 using namespace std;
 
 void string_handler(string identifier, string payload){
-	cout << identifier << ": " << payload << " fired" << endl;
+   cout << identifier << ": " << payload << " fired" << endl;
 }
 
 void int_handler(string identifier, int payload){
-	cout << identifier << ": " << payload << " fired" << endl;
+   cout << identifier << ": " << payload << " fired" << endl;
 }
 
 int main(){
-	EventEmitter<string> stringEmitter;
-	
-	stringEmitter.addListener("deeply.nested.test", boost::bind(string_handler, "string will fire #1", _1));
-	stringEmitter.addListener("deeply.*", 				boost::bind(string_handler, "string will fire #2", _1));
-	stringEmitter.addListener("deeply.*.test", 		boost::bind(string_handler, "string will fire #3", _1));
-	stringEmitter.addListener("deeply.*.bad", 		boost::bind(string_handler, "string won't fire", _1));
-	
-	stringEmitter.emit("deeply.nested.test", "This is the payload .. ");
-	
-	EventEmitter<int> intEmitter;
-	
-	intEmitter.addListener("this.is.a.channel",  boost::bind(int_handler, "int will fire #1", _1));
-	intEmitter.addListener("this.*", 				boost::bind(int_handler, "int will fire #2", _1));
-	intEmitter.addListener("this.*.channel", 		boost::bind(int_handler, "int will fire #3", _1));
-	intEmitter.addListener("aint.gonna.*", 	   boost::bind(int_handler, "int won't fire", _1));
-	
-	intEmitter.emit("this.is.a.channel", 42);
-		
+   EventEmitter<string> stringEmitter;
+   
+   stringEmitter.addListener("deeply.nested.test", boost::bind(string_handler, "string will fire #1", _1));
+   stringEmitter.addListener("deeply.*",           boost::bind(string_handler, "string will fire #2", _1));
+   stringEmitter.addListener("deeply.*.test",      boost::bind(string_handler, "string will fire #3", _1));
+   stringEmitter.addListener("deeply.*.bad",       boost::bind(string_handler, "string won't fire", _1));
+   
+   stringEmitter.emit("deeply.nested.test", "This is the payload .. ");
+   
+   EventEmitter<int> intEmitter;
+   
+   intEmitter.addListener("this.is.a.channel",  boost::bind(int_handler, "int will fire #1", _1));
+   intEmitter.addListener("this.*",             boost::bind(int_handler, "int will fire #2", _1));
+   intEmitter.addListener("this.*.channel",     boost::bind(int_handler, "int will fire #3", _1));
+   intEmitter.addListener("aint.gonna.*",       boost::bind(int_handler, "int won't fire", _1));
+   
+   intEmitter.emit("this.is.a.channel", 42);
+      
    return 0;
 }
